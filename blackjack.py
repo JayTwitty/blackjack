4486619584a4bc8be79e07e2b3f796d64abcc620
JayTwitty <jamariot@yahoo.com>
2016-02-11 09:11:00 -0500

    Blackjack game

--- blackjack.py
+++ blackjack.py
@@ -15,9 +15,9 @@ for card_suit in cards:
         shuffled_cards.append(card)
 
 random.shuffle(shuffled_cards)
-# print(shuffled_cards)
 
 # Dealing two cards in the deck for the player
+
 player_real_hand = []
 player_real_hand.append(shuffled_cards[:2])
 print("Player hand... ")
@@ -25,29 +25,28 @@ print(player_real_hand)
 print("")
 
 # Dealing the next two cards in the deck for the dealer
+
 dealer_real_hand = []
 dealer_real_hand.append(shuffled_cards[2:4])
 print("Dealer hand... ")
 print(dealer_real_hand)
-print("")
 
 # getting the values of the player cards into a list
+
 player_hand = []
 for card in player_real_hand:
     for value in card:
         player_hand.append(value[0])
-#print(player_hand)
 
 # getting the values of the dealer cards into a list
 dealer_hand = []
 for card in dealer_real_hand:
     for value in card:
         dealer_hand.append(value[0])
-#print(dealer_hand)
 
 print("=" * 20)
 
-#Adding up the value of the players cards.
+# Adding up the value of the players cards.
 player_hand_value = 0
 
 for item in player_hand:
@@ -70,6 +69,7 @@ else:
     print("Player Hand Value...")
     print(player_hand_value)
 
+# Adding up the values in the dealer's hand
 dealer_hand_value = 0
 
 for item in dealer_hand:
@@ -95,8 +95,8 @@ else:
 
 print("=" * 20)
 while player_hand_value < 21:
-# Dealing two cards in the deck for the player
-    player_decision = input("Player press H for 'Hit' and press S for 'stay':").lower()
+# Continuing the game for the player
+    player_decision = input("Player press H for 'Hit' and press S for 'stay': ").lower()
     print("=" * 20)
     if player_decision == "h":
         player_real_hand.append(shuffled_cards[5:6])
@@ -105,15 +105,16 @@ while player_hand_value < 21:
         print("")
     else:
         pass
+    player_hand = []
+    for card in player_real_hand:
+        for value in card:
+            player_hand.append(value[0])
     if player_hand_value > 21:
         print("Player Hand Value...")
         print(player_hand_value)
-        print("Busted!")
-    elif dealer_hand_value < 17:
-        dealer_real_hand.append(shuffled_cards[7:8])
-        print("Dealer hand... ")
-        print(dealer_real_hand)
-        print("")
+        print("Busted! You lose")
+        break
+    player_hand_value = 0
     for item in player_hand:
         if item in ["King", "Queen", "Jack"]:
             player_hand_value += 10
@@ -124,6 +125,18 @@ while player_hand_value < 21:
                 player_hand_value += 11
         else:
             player_hand_value += item
+    if dealer_hand_value < 17:
+        dealer_real_hand.append(shuffled_cards[7:8])
+        print("Dealer hand... ")
+        print(dealer_real_hand)
+        print("")
+    else:
+        print("Dealer Stays...")
+    dealer_hand = []
+    for card in dealer_real_hand:
+        for value in card:
+            dealer_hand.append(value[0])
+    dealer_hand_value = 0
     for item in dealer_hand:
         if item in ["King", "Queen", "Jack"]:
             dealer_hand_value += 10
